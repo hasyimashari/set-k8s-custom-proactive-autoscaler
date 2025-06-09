fbeb415ea4bfc8b47d1e348bead11572cf1ca9f6
hasyimashari <hasyim11c@protonmail.com>
2025-06-09 19:39:12 +0700

    add simple test web

---
+++ web-foo/foo.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "code": "200",
+        "status":"OK"
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
